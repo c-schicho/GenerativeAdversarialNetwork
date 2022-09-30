b1975b7db474cbcac7c1fcf951652777d841a973
Christopher Schicho <57227765+c-schicho@users.noreply.github.com>
2022-09-30 12:09:06 +0200

    #2-create the fnn discriminator model (#8)

---
+++ gan/discriminator.py
@@ -0,0 +1,27 @@
+import torch
+from torch.nn import Module, Sequential, Linear, LeakyReLU, Sigmoid
+
+
+class FNNDiscriminator(Module):
+
+    def __init__(self, in_dims: int):
+        super(FNNDiscriminator, self).__init__()
+        self.discriminator = Sequential(
+            Linear(in_dims, 1024),
+            LeakyReLU(.2),
+            Linear(1024, 512),
+            LeakyReLU(.2),
+            Linear(512, 256),
+            LeakyReLU(.2),
+            Linear(256, 1),
+            Sigmoid()
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.discriminator(x)
+
+    def save(self, path: str):
+        torch.save(self.discriminator.state_dict(), path)
+
+    def load(self, path: str):
+        self.discriminator.load_state_dict(torch.load(path))
